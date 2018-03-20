cf83e7f129c48aa7e72edc20bad1daff99c449e5
JESSYBJA <35872971+JESSYBJA@users.noreply.github.com>
2018-03-20 14:32:34 -0600

    ejercicio
    ejercicio1

---
+++ ejercicio1/ejercicio1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double v = 0;
+            double t = 0;
+            double calculo;
+            Console.WriteLine("ingrese un numero para calcular la velocidad");
+            t = Convert.ToInt32(Console.ReadLine());
+            v =  100 /t ;
+            calculo = v;
+
+            
+            Console.WriteLine("La velocidad es igual a:" + calculo);
+            Console.ReadKey();
+        }
+    }
+}
