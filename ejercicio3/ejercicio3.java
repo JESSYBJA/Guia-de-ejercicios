170c7c585aa1b4b65ff5c26ecc1ce3d35a6bfdcb
JESSYBJA <35872971+JESSYBJA@users.noreply.github.com>
2018-03-20 19:23:39 -0600

    ejercicio3
    ejercicio3 Java

---
+++ ejercicio3/ejercicio3.java
@@ -0,0 +1,11 @@
+public class ejercicio3{
+ public static void main(String args[]){
+double volaje= 250;
+double resistencia = 100;
+double resultado;
+double corriente;
+corriente= 250/100;
+resultado = corriente;
+System.out.println("el resultado de la corriente es:"+ resultado);
+}
+}
